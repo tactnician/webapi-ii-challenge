50300c00ec0c3476cc9b4f728849bb6e2f3b73f6
tactnician <tactnician@gmail.com>
2019-03-26 16:59:54 -0400

    initial commit - server up

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const server = express();
+
+// const postsRouter = require('./posts/posts-router');
+
+server.use(express.json());
+
+server.get('/', (req, res)=>{
+    res.send(`
+        <div style="background-image: url('https://i.redd.it/lwwt86ci5anz.jpg'); background-size: cover; height: 700px; ">
+            <h2 style="color:white;"> Post Portal </h2>
+        </div>
+    `);
+})
+
+// server.user('/api/posts', postsRouter);
+
+module.exports = server; 
