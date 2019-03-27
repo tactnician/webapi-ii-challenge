6123565ab06b8fc92b51ce5d1d21d089815554eb
tactnician <tactnician@gmail.com>
2019-03-26 20:00:42 -0400

    fleshed out all crud operations - debugging

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -36,7 +36,14 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const post = await Posts.insert(req.body);
-        res.status(201).json(post);
+        console.log(post);
+        if(post){
+            res.status(201).json(post);
+        } else {
+            res
+                .status(404)
+                .json({errorMessage: "Please provide title and contents for the post." })
+        }
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -45,9 +52,23 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/:id', async (req, res) => {});
+router.put('/:id', async (req, res) => {
 
-router.delete('/:id', async (req, res) => {});
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const count = await Posts.remove(req.params.id);
+        console.log(' delete obj ', post)
+        if(count > 0){
+            res.status(200).json({message: "the post has been removed"})
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    } catch (error) {
+        res.status(500).json({error: "The post could not be removed" });
+    }
+});
 
 
 module.exports = router; 
